59b4a36d1ff558cae71e88883f4101c51e1fcb26
Jabalus <jabalus@stratpoint.com>
2017-10-06 13:24:09 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,2 @@
+console.log('Hello from Webpack');
+require("./assets/styles/app.less");
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: './bundle.js'
+    },
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+        }]
+    }
+};
